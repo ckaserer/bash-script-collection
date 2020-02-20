62f662a4f13ae5c9331117c1717332503f10da96
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-02-20 09:22:23 +0100

    updated execute function with whitespace trimming

--- functions/execute.sh
+++ functions/execute.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# execute $COMMAND [$DRYRUN=false]
-# if command and dryrun=true are provided the command will be execuded
-# if command and dryrun=false (or no second argument is provided) 
+# execute $COMMAND [$ALT_TEXT] [$FLAG_DRYRUN=false]
+# if command and FLAG_DRYRUN=true are set the command will be execuded
+# if command and FLAG_DRYRUN=false (or no 3rd argument is provided) 
+# if an ALT_TEXT is provided it will print the alt text instead of the command
+# this can be used to mask sensitiv information
 # the function will only print the command the command to stdout
 function execute () {
   local exec_command=$1
@@ -10,10 +12,10 @@ function execute () {
   local flag_dryrun=${3:-$FLAG_DRYRUN}
 
   if [[ "${flag_dryrun}" == false ]]; then
-    echo "+ ${alt_text}"
+    echo "+ ${alt_text}" | sed 's/  */ /g'
     eval "${exec_command}"
   else
-    echo "${exec_command}"
+    echo "${exec_command}" | sed 's/  */ /g'
   fi
 }
 # readonly definition of a function throws an error if another function 
